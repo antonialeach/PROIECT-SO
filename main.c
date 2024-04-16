5e6f429e8b6c808b981be01f53dc9b87fd140f3b
antonia <antonia@antonia.myguest.virtualbox.org>
2024-04-16 22:50:11 +0300

    print directory structure

--- main.c
+++ main.c
@@ -1,41 +1,53 @@
 #include <stdio.h>
 #include <string.h>
 #include <dirent.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdlib.h>
 #include <linux/limits.h>
 
-void listFiles(const char *dirname) {
+void parse_directory(const char *dirname, int depth) {
     DIR *dir = opendir(dirname);
     if (dir == NULL) {
+        printf("Error at opening directory\n");
         return;
     }
 
-    printf("Reading files in %s:\n", dirname);
-
     struct dirent *entity;
     while ((entity = readdir(dir)) != NULL) {
-        printf("%s\n", entity->d_name);
-        printf("%d\n", entity->d_type);
+        if (strcmp(entity->d_name, ".") == 0 || strcmp(entity->d_name, "..") == 0) {
+            continue;
+        }
 
-        if (entity->d_type == 4 && strcmp(entity->d_name, ".") != 0 && strcmp(entity->d_name, "..") != 0) {
-            char path[PATH_MAX]; // Ensure enough space for the path
-            snprintf(path, sizeof(path), "%s/%s", dirname, entity->d_name);
-            listFiles(path);
+        for (int i = 0; i < depth; ++i) {
+            printf("|   ");
+        }
+
+        printf("|-- %s", entity->d_name);
+
+        char path[PATH_MAX];
+        snprintf(path, sizeof(path), "%s/%s", dirname, entity->d_name);
+        struct stat st;
+        if (stat(path, &st) == 0) {
+            if (S_ISDIR(st.st_mode)) {
+                printf(" (Directory)\n");
+                parse_directory(path, depth + 1);
+            } else if (S_ISREG(st.st_mode)) {
+                printf(" (File)\n");
+            } else {
+                printf(" (Other)\n");
+            }
+        } else {
+            printf(" (Error getting information)\n");
         }
     }
 
     closedir(dir);
 }
 
-int main(int argc, char* argv[]) {
-    
-    if (argc != 2) {
-        printf("Usage: %s <directory>\n", argv[0]);
-        return 1;
-    }
-
-    const char *path = argv[1];
-
-    listFiles(path);
-
+int main() {
+    parse_directory("/home/antonia/Desktop/dir exemple", 0); 
     return 0;
 }
+
