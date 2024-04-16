148b019f00dcd2c99203471726549fde044f98bd
antonia <antonia@antonia.myguest.virtualbox.org>
2024-04-16 23:33:18 +0300

    print directory structure + reading from command line

--- main.c
+++ main.c
@@ -46,8 +46,15 @@ void parse_directory(const char *dirname, int depth) {
     closedir(dir);
 }
 
-int main() {
-    parse_directory("/home/antonia/Desktop/dir exemple", 0); 
+int main(int argc, char **argv) {
+
+    if(argc != 2)
+    {
+        printf("Uncorrect number of arguments");
+        return 1;
+    }
+
+    parse_directory(argv[1], 0); 
     return 0;
 }
 
